604e8283b9cb4999ebbde509c509c8b95c9beb6c
fammu03 <fammu03@gmail.com>
2023-04-28 20:30:48 +0530

    modified code

--- new.js
+++ new.js
@@ -1,5 +1,49 @@
 console.log("Hello World!");
-var n1 = prompt("Enter multiplicand: ");
-var n2 = prompt("Enter multiplier: ");
-alert("Multiplicand: "+n1);
-alert("Multiplier: "+n2);
+function importData() {
+    var n1 = document.getElementById("n1").value;
+    console.log("The user entered: "+n1);
+    var n2 = document.getElementById("n2").value;
+    console.log("The user entered: "+n2);
+    return n1, n2;
+}
+
+function rev(s) {
+    return s.split('').reverse().join('');
+}
+
+function tcmp(n) {
+
+}
+
+function dtbs(n) {
+    let s='';
+    if(n>=0) {
+        while (n!=0) {
+            let r = n%2;
+            n = parseInt(n/2);
+            s += String(r);
+        }
+        s += '0';
+        s = rev(s);
+    }
+    return s;
+}
+
+function badd(n, m) {
+
+}
+
+function asr(n) {
+
+}
+
+function equalize(n,m) {
+
+}
+
+function mul(m, q) {
+
+}
+
+var n1, n2 = importData();
+document.getElementById("p").innerHTML += dtbs(n1);
