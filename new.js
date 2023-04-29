3a066657615fa6f77ea6046fea3b8fe4cfa33120
fammu03 <fammu03@gmail.com>
2023-04-29 05:54:10 +0530

    fixed the return issues and addded equalize and 2's complement

--- new.js
+++ new.js
@@ -4,7 +4,7 @@ function importData() {
     console.log("The user entered: "+n1);
     var n2 = document.getElementById("n2").value;
     console.log("The user entered: "+n2);
-    return n1, n2;
+    return {n1, n2};
 }
 
 function rev(s) {
@@ -12,19 +12,45 @@ function rev(s) {
 }
 
 function tcmp(n) {
+    let s = rev(n),
+        f = 0,
+        i = 0;   
+    while (f==0) {
+        if (s.charAt(i) == '1') {
+            console.log("Found you! @"+i);
+            f++;
+        }
+        i++;
+    }
 
+    while (i!=s.length) {
+        if (s[i] == '0') {
+            s =s.slice(0,i)+'1'+s.slice(i+1,s.length);
+        }
+        else if (s[i] == '1') {
+            s =s.slice(0,i)+'0'+s.slice(i+1,s.length);
+        }
+        i++;
+    }
+    s = rev(s);
+    console.log("comp: "+s);
+    return s;
 }
 
 function dtbs(n) {
-    let s='';
+    var s='';
     if(n>=0) {
         while (n!=0) {
             let r = n%2;
             n = parseInt(n/2);
             s += String(r);
         }
-        s += '0';
+        s = s+'0';
         s = rev(s);
+    }
+
+    else {
+        s = tcmp(dtbs(Math.abs(n)))
     }
     return s;
 }
@@ -38,12 +64,31 @@ function asr(n) {
 }
 
 function equalize(n,m) {
+    while(n.length != m.length) {
+        if (n.length < m.length) {
+            n = n[0]+n;
+        }
 
+        else {
+            m = m[0]+m;
+        }
+    }
+    return {n, m};
 }
 
 function mul(m, q) {
-
+    var n= importData();
+    n1 = n.n1;
+    n2 = n.n2;
+    n1 = dtbs(n1)
+    n2 = dtbs(n2)
+    n = equalize(n1, n2);
+    n1 = n.n;
+    n2 = n.m;
+    document.getElementById("p").innerHTML += n1+"<br>"+n2;
 }
 
-var n1, n2 = importData();
-document.getElementById("p").innerHTML += dtbs(n1);
+//tcmp('1010');
+
+
+//dtbs('10');
